57479fe466f815ff236e1dbf595df9aca2555098
Marco Villalta <mvillal1@terpmail.umd.edu>
2024-12-06 00:12:42 -0500

    Added map to adopt now page

--- public/adopt-now.js
+++ public/adopt-now.js
@@ -1,3 +1,9 @@
+// API Key configuration
+const API_KEY = '6fl4zm3w';
+
+// Map global declaration
+let map;
+
 async function createAdopter() {
     console.log('Creating Adopter');
     await fetch(`${host}/adopter`, {
@@ -17,4 +23,47 @@ async function createAdopter() {
         'Content-Type': 'application/json',
       },
     }).then((res) => res.json());
+}
+
+async function fetchOrgs(e) {
+  if(e) {
+    e.preventDefault();
   }
+
+  const searchZipCode = document.getElementById('searchZip').value
+  const radius = document.getElementById('miles').value
+
+  try {
+    const response = await fetch(`https://api.rescuegroups.org/v5/public/orgs/?filterRadius[postalcode]=${searchZipCode}&filterRadius[miles]=${radius}`, {
+        headers: {
+            'Content-Type': 'application/vnd.api+json',
+            'Authorization': API_KEY
+        }
+    });
+    const data = await response.json();
+    const organizations = data.data
+    organizations.forEach(org => {
+      const lat = org.attributes.lat
+      const lon = org.attributes.lon
+      const orgName = org.attributes.name
+      const orgType = org.attributes.type
+      const orgEmail = org.attributes.email
+
+      const details = `<b style='font-size:15pt;'> ${orgName} </b> <br> <i style='font-size:12pt;'> Type: ${orgType} </i> <br>${orgEmail}`
+      L.marker([lat, lon]).addTo(map).bindPopup(details)
+    });
+  } catch (error) {
+      console.error('Error fetching organizations:', error);
+  }
+}
+
+function createMap() {
+  map = L.map('map').setView([37.5, -95], 4);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 50,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+}
+
+window.onload = createMap();
